d826dd245162047caf601f38fe8b83f8d59f1d90
Tony Ito-Cole <tonyitocole@gmail.com>
2020-02-25 20:50:58 -0800

    Completed middleware and router for receiving get request for specific location ID

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -2,21 +2,80 @@ const db = require('../models/compostModels');
 
 const mainController = {};
 
-
-//controller to get all items
-mainController.getItems = (req, res, next) => {
-    const query = 'SELECT * FROM items';
-    db.query(query)
-    .then ( data => {
-      res.locals.items = data.rows;
+//controller to get info from a selected page
+mainController.getPageUnique = (req, res, next) => {
+  const query = `SELECT * FROM pages WHERE param='${req.params.id}' `;
+  db.query(query)
+    .then(data => {
+      res.locals.info = {
+        date: data.rows[0].date,
+        brief: data.rows[0].brief,
+        title: data.rows[0].title,
+        location: data.rows[0].location
+      };
+      res.locals.locationID = data.rows[0].id;
       next();
-    }
-    )
-    .catch (err => next({
-      log: `Express error handler caught getItems error ${err}`,
-      status: 400,
-      message: { err: `${err}`},
-    }))
+    })
+    .catch(err =>
+      next({
+        log: `Express error handler caught getItems error ${err}`,
+        status: 400,
+        message: { err: `${err}` }
+      })
+    );
 };
+
+//controller to get list of items from a selected page
+mainController.getList = (req, res, next) => {
+  const query = `SELECT items.id AS id, items.name AS item_name, users.name AS user_name FROM items 
+                INNER JOIN pages ON items.page_id = ${res.locals.locationID} 
+                INNER JOIN users ON items.user_id = users.id;`;
+  db.query(query)
+    .then(data => {
+      const output = [];
+      for (value of data.rows) {
+        output.push({
+          id: value.id,
+          user: value.user_name,
+          name: value.item_name
+        });
+      }
+      res.locals.list = output;
+      next();
+    })
+    .catch(err =>
+      next({
+        log: `Express error handler caught getList error ${err}`,
+        status: 400,
+        message: { err: `${err}` }
+      })
+    );
+};
+
+//controller to get users from a selected page
+mainController.getUsers = (req, res, next) => {
+    const query = `SELECT * FROM users WHERE page_id = ${res.locals.locationID};`;
+    db.query(query)
+      .then(data => {
+        const output = {};
+        for (value of data.rows) {
+            const temp = {
+                phone: value.phone,
+                email: value.email,
+                name: value.name
+            }
+            output[value.id] = temp;
+        }
+        res.locals.users = output;
+        next();
+      })
+      .catch(err =>
+        next({
+          log: `Express error handler caught getUsers error ${err}`,
+          status: 400,
+          message: { err: `${err}` }
+        })
+      );
+  };
 
 module.exports = mainController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,14 +4,21 @@ const mainController = require('../controllers/mainController.js');
 
 const router = express.Router();
 
+//route all get requests to /api/:id here
+router.get('/:id', mainController.getPageUnique, mainController.getList, mainController.getUsers, (req, res) => {
+    const response = {
+        information: res.locals.info,
+        list: res.locals.list,
+        users: res.locals.users
+    };
+  res.status(200).json(response)
+}
+);
+
 //route all post requests to /api/ here
 router.post('/', (req, res) =>
   res.status(200)
 );
 
-//route all get requests to /api/ here
-router.get('/', mainController.getItems, (req, res) =>
-  res.status(200).json(res.locals.items)
-);
 
 module.exports = router;
