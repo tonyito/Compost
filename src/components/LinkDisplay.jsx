217df78b8cb38c946ecdf65fd78afebc0556d097
sean arseneault <itsmesean88@gmail.com>
2020-02-26 16:01:35 -0800

    Built page creation feature

---
+++ src/components/LinkDisplay.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const LinkDisplay = props => {
+  return (
+    <div>
+      <Dialog
+        open={props.open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={props.handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default LinkDisplay;
--- src/home.jsx
+++ src/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -7,11 +7,15 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
+import moment from 'moment';
+import { DatePicker } from '@material-ui/pickers';
+import LinkDisplay from './components/LinkDisplay';
 
 const useStyles = makeStyles({
   root: {
     width: 650,
-    height: 435,
+    height: 440,
   },
   title: {
     fontSize: 26,
@@ -20,10 +24,44 @@ const useStyles = makeStyles({
     marginBottom: 12,
   },
 });
+
 const Home = () => {
+  const [url, setUrl] = useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const classes = useStyles();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const obj = {
+      title: event.target.title.value,
+      description: event.target.description.value,
+      location: event.target.location.value,
+      date: `${moment(event.target.startDate.value).format(
+        'MM/DD/YYYY',
+      )} - ${moment(event.target.endDate.value).format('MM/DD/YYYY')}`,
+    };
+    fetch('/api/pages', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(obj),
+    })
+      .then(res => res.json())
+      .then(data => {
+        handleClickOpen();
+        setUrl(data);
+      });
+  };
+
   return (
-    <React.Fragment style={{ backgroundColor: '#eeeeee' }}>
+    <div style={{ backgroundColor: '#eeeeee' }}>
       <Typography variant="h3" align="center">
         {'Compost'}
       </Typography>
@@ -36,68 +74,80 @@ const Home = () => {
       >
         <div style={{ marginBottom: '25px' }}>
           <Card className={classes.root} variant="outlined">
-            <CardContent style={{ display: 'flex', flexDirection: 'column' }}>
-              <Typography
-                className={classes.title}
-                color="textPrimary"
-                gutterBottom
-              >
-                Create your adventure
-              </Typography>
-              <TextField
-                style={{ width: '30vh' }}
-                variant="outlined"
-                defaultValue="Title"
-              />
-              <TextField
-                style={{ margin: '10px 0 10px 0' }}
-                id="outlined-multiline-static"
-                multiline
-                rows="4"
-                defaultValue="Description"
-                variant="outlined"
-              />
-              <TextField
-                style={{ width: '30vh', marginBottom: '10px' }}
-                variant="outlined"
-                defaultValue="Location"
-              />
-              <div style={{ display: 'flex' }}>
+            <form onSubmit={handleSubmit}>
+              <CardContent style={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography
+                  className={classes.title}
+                  color="textPrimary"
+                  gutterBottom
+                >
+                  Create your adventure
+                </Typography>
                 <TextField
-                  style={{ width: '200px' }}
-                  id="startDate"
-                  label="Start date"
-                  type="date"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
+                  id="title"
+                  style={{ width: '30vw' }}
+                  variant="outlined"
+                  placeholder="Title"
+                  required
                 />
                 <TextField
-                  style={{ width: '200px', marginLeft: '20px' }}
-                  id="endDate"
-                  label="End date"
-                  type="date"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
+                  style={{ margin: '10px 0 10px 0', width: '30vw' }}
+                  id="description"
+                  multiline
+                  rows="4"
+                  placeholder="Description"
+                  variant="outlined"
+                  required
                 />
-              </div>
-              <Button
-                style={{ marginTop: '10px' }}
-                variant="contained"
-                type="submit"
-                color="primary"
-              >
-                Lets Go!
-              </Button>
-            </CardContent>
+                <TextField
+                  id="location"
+                  style={{ width: '30vw', marginBottom: '10px' }}
+                  variant="outlined"
+                  placeholder="Location"
+                  required
+                />
+                <div style={{ display: 'flex' }}>
+                  <TextField
+                    style={{ width: '200px' }}
+                    id="startDate"
+                    label="Start date"
+                    type="date"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    defaultValue={moment(new Date()).format('YYYY-MM-DD')}
+                    required
+                  />
+                  <TextField
+                    style={{ width: '200px', marginLeft: '20px' }}
+                    id="endDate"
+                    label="End date"
+                    type="date"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    defaultValue={moment(new Date()).format('YYYY-MM-DD')}
+                    required
+                  />
+                </div>
+                <Button
+                  style={{ marginTop: '10px' }}
+                  variant="contained"
+                  type="submit"
+                  color="primary"
+                >
+                  Lets Go!
+                </Button>
+                <LinkDisplay open={open} handleClose={handleClose} />
+              </CardContent>
+            </form>
           </Card>
         </div>
         <div>
           <Card className={classes.root} variant="outlined"></Card>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
