62af2a653bca807ba78deeb4c07176d53941f5b8
Tony Ito-Cole <tonyitocole@gmail.com>
2020-02-27 16:06:25 -0800

    Changed delete middleware to take an array

--- server/controllers/deleteController.js
+++ server/controllers/deleteController.js
@@ -3,35 +3,36 @@ const db = require('../models/compostModels');
 const deleteController = {};
 
 //controller to delete a user from a selected page
-deleteController.deleteUser = (req, res, next) => {
-    const query = `UPDATE users SET active = false WHERE id = $1`;
-    db.query(query, [req.body.id])
-      .then(() => {
-        return next();
-      })
-      .catch(err => {
-        return next({
-          log: `Express error handler caught deleteUser error ${err}`,
-          status: 400,
-          message: { err: `${err}` },
-        });
-      });
-  };
+deleteController.deleteUser = async (req, res, next) => {
+  try {
+    for (value of req.body.id) {
+      const query = `UPDATE users SET active = false WHERE id = $1`;
+      const run = await db.query(query, [value]);
+    }
+    return next();
+  } catch (err) {
+    return next({
+      log: `Express error handler caught deleteUser error ${err}`,
+      status: 400,
+      message: { err: `${err}` },
+    });
+  }
+};
 
 //controller to delete an item from a selected page
 deleteController.deleteItem = (req, res, next) => {
-    const query = `DELETE FROM items WHERE id = $1`;
-    db.query(query, [req.body.id])
-      .then(() => {
-        return next();
-      })
-      .catch(err => {
-        return next({
-          log: `Express error handler caught deleteItem error ${err}`,
-          status: 400,
-          message: { err: `${err}` },
-        });
+  const query = `DELETE FROM items WHERE id = $1`;
+  db.query(query, [req.body.id])
+    .then(() => {
+      return next();
+    })
+    .catch(err => {
+      return next({
+        log: `Express error handler caught deleteItem error ${err}`,
+        status: 400,
+        message: { err: `${err}` },
       });
-  };
-  
-  module.exports = deleteController;
+    });
+};
+
+module.exports = deleteController;
